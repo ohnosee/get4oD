ddc747567407661297c88367ea1268f176c59deb
Andrew Jackson <andrew@andresworld.co.uk>
2014-04-08 14:09:13 -0400

    DB write for thirty day spider

--- get4od/get4od/spiders/thirtydayschedule.py
+++ get4od/get4od/spiders/thirtydayschedule.py
@@ -2,8 +2,9 @@ import datetime
 #framework
 from scrapy.spider import BaseSpider
 from scrapy.selector import HtmlXPathSelector
+from scrapy.http import Request
 #module
-from get4od.items import Get4OdItem
+from get4od.items import Get4OdItem, ProgrammeItem
 
 class ScheduleSpider(BaseSpider):
     name = "schedule_date"
@@ -34,7 +35,7 @@ class ScheduleSpider(BaseSpider):
             progs = m.select("ul/li")
             for p in progs:
                 if not p.select("@class").extract()[0] == 'unavailable':
-                    item = Get4OdItem()
+                    item = ProgrammeItem()
                     item['wsprogrammeid'] = p.select("@data-wsprogrammeid").extract()[0]
                     item['channel'] = m.select("h2/text()").extract()[0]
                     item['link'] = p.select("a[@class='promo-link']/@href").extract()[0]
@@ -56,5 +57,36 @@ class ScheduleSpider(BaseSpider):
                     url = response.url
                     date = '-'.join(num for num in url.split('/')[-4:-1])
                     item['date'] =  date
-                    items.append(item)
-        return items
+                    #items.append(item)
+                    proglink = 'http://www.channel4.com' + item['link']
+                    request = Request(proglink, callback=self.parseprog, dont_filter=True)
+                    request.meta['item'] = item
+                    yield request
+        #yield items
+        
+    def parseprog(self, response):
+        item = response.meta['item']
+        wsprogrammeid = item['wsprogrammeid']
+        hxs = HtmlXPathSelector(response)
+        try:
+            data = hxs.select('//li[@data-wsprogrammeid="%s"]' % wsprogrammeid)[0]
+            item['series_number'] = data.select("@data-series-number").extract()[0]
+            item['archiveepisode'] = data.select("@data-archiveepisode").extract()[0]
+            item['episode_number'] = data.select("@data-episode-number").extract()[0]
+            item['assetid'] = data.select("@data-assetid").extract()[0]
+            item['audiodesc_assetid'] = data.select("@data-audiodesc-assetid").extract()[0]
+            item['episodeurl'] = data.select("@data-episodeurl").extract()[0]
+            item['requireslogin'] = data.select("@data-requireslogin").extract()[0]
+            item['image_url'] = data.select("@data-image-url").extract()[0]
+            item['compliance'] = data.select("@data-compliance").extract()[0]
+            #item['txtime'] = data.select("@data-txtime").extract()[0]
+            item['txday'] = data.select("@data-txday").extract()[0]
+            item['txdate'] = data.select("@data-txdate").extract()[0]
+            item['channellogo'] = data.select("@data-channellogo").extract()[0]
+            item['episodeduration'] = data.select("@data-episodeduration").extract()[0]
+            item['episodetitle'] = data.select("@data-episodetitle").extract()[0]
+            item['episodeinfo'] = data.select("@data-episodeinfo").extract()[0]
+            return item
+        except IndexError:
+            item['scrape_error'] = "Programme expired"
+            return item
